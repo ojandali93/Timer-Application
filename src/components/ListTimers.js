e4db118dd69dad9e2b7a4e698d8bd638a8aa8e03
omar jandali <omarjandali93@gmail.com>
2023-05-08 16:28:43 -0700

    added timer view

--- src/components/ListTimers.js
+++ src/components/ListTimers.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import TimerView from '../TimerView'
+
 export default function ListTimers() {
 	const timers = useSelector(state => state.timers.value)
-
+	
 	return (
 		<div>
-			{timers.map((timer, i) => {
-				return (
-					<div>
-						<h2>{timer.name}</h2>
-						<h1>{timer.time}</h1>
-						<button>Start</button>
-					</div>
-				)
-			})}
+			{timers.map((timer, i) => <TimerView key={`timer-${i}`} {...timer} />)}
 		</div>
 	)
 }
