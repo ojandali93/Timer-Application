0695f879bca9ce27c203025a0c02075bf7ce4686
omar jandali <omarjandali93@gmail.com>
2023-05-08 19:05:37 -0700

    presisting timers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import NewTimer from './components/NewTimer';
 import ListTimers from './components/ListTimers';
-import { update } from './features/timers/timersSlice';
 
 function App() {
   return (
